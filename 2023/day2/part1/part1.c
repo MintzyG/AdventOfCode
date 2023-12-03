d93727c5fd8125a4ba06f8410dbe170de4b901f9
MintzyG <ericbraga2000@gmail.com>
2023-12-02 22:49:55 -0300

    Cleaned up code for day 2 2023

--- 2023/day2/part1/part1.c
+++ 2023/day2/part1/part1.c
@@ -18,14 +18,11 @@ int main() {
   while (1) {
     getline(&line, &bufsize, file);
     game = strstr(line, ":");
-    // puts(line);
-    // puts(game);
+    id += 1;
 
     if(feof(file)){ break; }
-
-    id += 1;
     while (1) {
-      while (*game) {
+      while (*game)
         if (isdigit(*game)) {
           validator = strtoul(game, NULL, 10);
           break;
@@ -33,35 +30,22 @@ int main() {
           game += 1;
           if (game[0] == '\n') { answer += id; /* printf("valid game\n") */; next = 1; break; }
         }
-      }
       if (next == 1) { next = 0; break;}
 
-      // printf("%d ", validator);
-      // Aligns color with text start for reading;
       char color;
-      if (validator - 10 >= 0) {
-        game += 3;
-        color = game[0];
-      } else {
-        game += 2;
-        color = game[0];
-      }
+      if (validator - 10 >= 0) { game += 3; color = game[0]; } 
+      else { game += 2; color = game[0]; }
 
-      // printf("%c ", color);
-      if (validator < 13) {
+      if (validator < 13)
         continue;
-      } else if (validator > 14) {
-        // printf("invalid game\n");
+      else if (validator > 14)
         break;
-      } else if (validator == 13 && color == 'r') {
-        // printf("invalid game\n");
+      else if (validator == 13 && color == 'r')
         break;
-      } else if (validator == 14 && color != 'b') {
-        // printf("invalid game\n");
+      else if (validator == 14 && color != 'b')
         break;
-      } else {
+      else
         continue;
-      }
     }
   }
   printf("Answer: %ld", answer);
--- 2023/day2/part2/part2.c
+++ 2023/day2/part2/part2.c
@@ -14,7 +14,6 @@ typedef struct set {
   Cube blue;
 }Set;
 
-
 int main() {
   FILE* file;
   file = fopen("in.txt", "r");
@@ -27,70 +26,43 @@ int main() {
   size_t answer = 0, bufsize = 64, next = 0, id = 0;
   char* line, *game;
   int validator = 0;
-
   while (1) {
     getline(&line, &bufsize, file);
     game = strstr(line, ":");
-    // puts(line);
-    // puts(game);
-
-    if(feof(file)){ break; }
-
     id += 1;
+
+    if(feof(file)) { break; }
     while (1) {
-      while (*game) {
+      while (*game)
         if (isdigit(*game)) {
           validator = strtoul(game, NULL, 10);
           break;
         } else {
           game += 1;
-          if (game[0] == '\n') { 
-            /* printf("valid game\n"); */ 
-            next = 1; 
-            break; 
-          }
+          if (game[0] == '\n') { next = 1; break; }
         }
-      }
       if (next == 1) { next = 0; break;}
 
-      // printf("%d ", validator);
-      // Aligns color with text start for reading;
       char color;
-      if (validator - 10 >= 0) {
-        game += 3;
-        color = game[0];
-      } else {
-        game += 2;
-        color = game[0];
-      }
+      if (validator - 10 >= 0) { game += 3; color = game[0]; } 
+      else { game += 2; color = game[0]; }
 
-      // keeps track of biggest number for each cube
       switch (color) {
         case 'r':
-          if (all_sets[id-1].red.amount < validator) {  
+          if (all_sets[id-1].red.amount < validator)
             all_sets[id-1].red.amount = validator;
-            // printf("%c%d ", color, validator);
-          }
           break;
         case 'g':
-          if (all_sets[id-1].green.amount < validator) {  
+          if (all_sets[id-1].green.amount < validator)
             all_sets[id-1].green.amount = validator;
-            // printf("%c%d ", color, validator);
-          }
           break;
         case 'b':
-          if (all_sets[id-1].blue.amount < validator) {  
+          if (all_sets[id-1].blue.amount < validator)
             all_sets[id-1].blue.amount = validator;
-            // printf("%c%d ", color, validator);
-          }
           break;
       }
     }
-    printf("\n");
-    int red = all_sets[id-1].red.amount; 
-    int green = all_sets[id-1].green.amount; 
-    int blue = all_sets[id-1].blue.amount;
-    answer += red * green * blue;
+    answer += all_sets[id-1].red.amount * all_sets[id-1].green.amount * all_sets[id-1].blue.amount;
   }
   printf("Answer: %ld", answer);
   return 0;
